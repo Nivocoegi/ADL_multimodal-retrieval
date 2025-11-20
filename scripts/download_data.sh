76c3db325fb1a76cdc2a4b221550aac05c599f32
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-20 15:58:32 +0100

    download_data.sh edited

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -3,7 +3,7 @@
 mkdir -p data/raw
 curl -L -o data/raw/flickr8k.zip \
   https://www.kaggle.com/api/v1/datasets/download/adityajn105/flickr8k
-unzip data/raw/flickr8k.zip -d data/raw/
+unzip data/raw/flickr8k.zip -d data/Images
 
 chmod +x scripts/download_data.sh
 
