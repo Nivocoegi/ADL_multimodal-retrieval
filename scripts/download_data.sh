a28d4dcd89266ab346e4358a35fa3bc6997b9209
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-20 16:04:07 +0100

    download_data.sh edited

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-mkdir -p data/raw
+mkdir -p /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/data
+
 curl -L -o data/raw/flickr8k.zip \
   https://www.kaggle.com/api/v1/datasets/download/adityajn105/flickr8k
-unzip data/raw/flickr8k.zip -d data/Images
+
+unzip /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/data/raw/flickr8k.zip -d /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/data
 
 chmod +x scripts/download_data.sh
 
