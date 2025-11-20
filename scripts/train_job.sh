2daee951f046e1f3da3bc752ea1c9b08726fac53
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-20 14:24:34 +0100

    submit and train_job.sh files added

---
+++ scripts/train_job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# ----------------------------
+# Module laden
+# ----------------------------
+module load gcc/9.4.0-pe5.344
+module load python/3.9.12-pe5.345
+module load cuda/12.1
+
+# ----------------------------
+# Conda initialisieren
+# ----------------------------
+source ~/miniconda3/etc/profile.d/conda.sh
+
+# ----------------------------
+# Environment erstellen (falls noch nicht vorhanden)
+# ----------------------------
+ENV_PATH="/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/.conda_env"
+ENV_YML="/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/notebooks/environment.yml"
+
+if [ ! -d "$ENV_PATH" ]; then
+    conda env create -f "$ENV_YML" -p "$ENV_PATH"
+fi
+
+# ----------------------------
+# Environment aktivieren
+# ----------------------------
+conda activate "$ENV_PATH"
+
+# ----------------------------
+# Training starten
+# ----------------------------
+python /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/notebooks/ADL_multimodel_retrieval.py
