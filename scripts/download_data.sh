9baf98c82ed333c787a0680d5deace2a13bb3432
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-20 15:49:11 +0100

    download_data file edited

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Erstellt Ordner und lädt Flickr8k herunter
-cd ..
-mkdir data/raw
-#wget https://github.com/jbrownlee/Datasets/releases/download/Flickr8k/Flickr8k_Dataset.zip -O data/raw/flickr8k.zip
-curl -L -o ~/ADL_multimodal_retrieval/data/raw/flickr8k.zip\
+
+mkdir -p data/raw
+curl -L -o data/raw/flickr8k.zip \
   https://www.kaggle.com/api/v1/datasets/download/adityajn105/flickr8k
 unzip data/raw/flickr8k.zip -d data/raw/
 
