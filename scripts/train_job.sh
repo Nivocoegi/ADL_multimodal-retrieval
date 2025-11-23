9c38b20102486fbbd73cd0efc3d1badcef56f5b6
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-23 10:38:09 +0100

    train_job.sh edited

--- scripts/train_job.sh
+++ scripts/train_job.sh
@@ -8,26 +8,13 @@ module load gcc/9.4.0-pe5.34
 module load USS/2022
 module load miniconda3/4.12.0
 
-
-
-
-
-
-# virtuellen Environment aktivieren
-source /cfs/software/uss/2022/spack/linux-rocky8-x86_64/gcc-9.4.0/miniconda3-4.12.0-7riiqd3uthjsmxqkabjrfhfxhyh5epcl/etc/profile.d/conda.sh
-conda activate /cfs/earth/scratch/vognic01/.conda/envs/ADL_multimodal-retrieval
-
-
-# ---- 3) Debug Info ----
+# ---- Debug Info ----
 echo "Python executable:"
-which python
-
-echo "Python version:"
-python --version
+/cfs/earth/scratch/vognic01/.conda/envs/ADL_multimodal-retrieval/bin/python --version
 
 echo "GPU Info:"
 nvidia-smi
 
-# ---- 4) Training starten ----
-python /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/notebooks/251123_ADL_multimodel_retrieval.py
-
+# ---- Training starten ----
+/cfs/earth/scratch/vognic01/.conda/envs/ADL_multimodal-retrieval/bin/python \
+/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/notebooks/251123_ADL_multimodel_retrieval.py
