7c7eb4ad7e0d520bcbaff9d75afb20150823af57
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-29 09:38:15 +0100

    earth partition changed > e5

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/out_%j.txt
 #SBATCH --error=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/err_%j.txt
 #SBATCH --time=24:00:00
-#SBATCH --partition=earth-4
+#SBATCH --partition=earth-5
 #SBATCH --gres=gpu:l40s:1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=32G
