933430ebcbf7bd89b0329057acf45ba642fa2cfa
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-29 17:32:40 +0100

    truncation bugfixed

--- cut_captions.py
+++ cut_captions.py
@@ -24,20 +24,56 @@
 # print("Truncated CSV gespeichert als flickr30k_captions_truncated.csv")
 
 
-import pandas as pd
+# import pandas as pd
+#
+# csv_file = "flickr30k_captions.csv"
+# df = pd.read_csv(csv_file, sep=";")  # anpassen, falls anderes Trennzeichen
+#
+# max_words = 60
+#
+# def truncate_caption_words(text):
+#     words = text.split()
+#     if len(words) > max_words:
+#         words = words[:max_words]
+#     return " ".join(words)
+#
+# df["caption"] = df["caption"].apply(truncate_caption_words)
+#
+# df.to_csv("flickr30k_captions_truncated.csv", sep=";", index=False)
+# print("Truncated CSV gespeichert als flickr30k_captions_truncated.csv")
+#
+# import pandas as pd
+# csv_file = "flickr30k_captions_truncated.csv"
+# df = pd.read_csv(csv_file, sep=";")
+#
+# # count length of captions in words
+# for index, row in df.iterrows():
+#     caption = row["caption"]
+#     word_count = len(caption.split())
+#     if word_count > 70:
+#         print(f"Caption {index} has {word_count} words.")
 
-csv_file = "flickr30k_captions.csv"
+
+import pandas as pd
+from transformers import CLIPTokenizer
+
+# CSV-Datei laden
+csv_file = "flickr30k_captions_truncated.csv"
 df = pd.read_csv(csv_file, sep=";")  # anpassen, falls anderes Trennzeichen
 
-max_words = 70
+# CLIP Tokenizer laden
+tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+# max_tokens = 77  # max_position_embeddings für CLIP
 
-def truncate_caption_words(text):
-    words = text.split()
-    if len(words) > max_words:
-        words = words[:max_words]
-    return " ".join(words)
+# def truncate_caption_tokens(text):
+#     tokens = tokenizer.encode(text, add_special_tokens=False)
+#     if len(tokens) > max_tokens:
+#         tokens = tokens[:max_tokens]
+#     return tokenizer.decode(tokens)
 
-df["caption"] = df["caption"].apply(truncate_caption_words)
+# # Captions kürzen
+# df["caption"] = df["caption"].apply(truncate_caption_tokens)
 
-df.to_csv("flickr30k_captions_truncated.csv", sep=";", index=False)
-print("Truncated CSV gespeichert als flickr30k_captions_truncated.csv")
+# Neue CSV speichern
+# df.to_csv("flickr30k_captions_truncated.csv", sep=";", index=False)
+# print("Truncated CSV gespeichert als flickr30k_captions_truncated.csv")
