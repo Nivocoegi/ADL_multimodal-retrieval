ded010f70773fb13b3f7e3069871c1dd9376b9d3
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-29 17:11:57 +0100

    captions cuted manualy w/o tokenizer

--- cut_captions.py
+++ cut_captions.py
@@ -1,24 +1,43 @@
+# import pandas as pd
+# from transformers import CLIPTokenizer
+#
+# # Pfad zur CSV
+# csv_file = "flickr30k_captions.csv"
+# df = pd.read_csv(csv_file, sep=";")  # falls andere Trennzeichen, anpassen
+#
+# # CLIP Tokenizer laden
+# tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
+# max_len = 70
+#
+# # Funktion zum Kürzen
+# def truncate_caption(text):
+#     tokens = tokenizer.encode(text, add_special_tokens=False)
+#     if len(tokens) > max_len:
+#         tokens = tokens[:max_len]
+#     return tokenizer.decode(tokens)
+#
+# # Captions kürzen
+# df["caption"] = df["caption"].apply(truncate_caption)
+#
+# # Neue CSV speichern
+# df.to_csv("flickr30k_captions_truncated.csv", sep=";", index=False)
+# print("Truncated CSV gespeichert als flickr30k_captions_truncated.csv")
+
+
 import pandas as pd
-from transformers import CLIPTokenizer
 
-# Pfad zur CSV
 csv_file = "flickr30k_captions.csv"
-df = pd.read_csv(csv_file, sep=";")  # falls andere Trennzeichen, anpassen
+df = pd.read_csv(csv_file, sep=";")  # anpassen, falls anderes Trennzeichen
 
-# CLIP Tokenizer laden
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
-max_len = 70
+max_words = 70
 
-# Funktion zum Kürzen
-def truncate_caption(text):
-    tokens = tokenizer.encode(text, add_special_tokens=False)
-    if len(tokens) > max_len:
-        tokens = tokens[:max_len]
-    return tokenizer.decode(tokens)
+def truncate_caption_words(text):
+    words = text.split()
+    if len(words) > max_words:
+        words = words[:max_words]
+    return " ".join(words)
 
-# Captions kürzen
-df["caption"] = df["caption"].apply(truncate_caption)
+df["caption"] = df["caption"].apply(truncate_caption_words)
 
-# Neue CSV speichern
 df.to_csv("flickr30k_captions_truncated.csv", sep=";", index=False)
 print("Truncated CSV gespeichert als flickr30k_captions_truncated.csv")
