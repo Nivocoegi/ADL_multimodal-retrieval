6ae802769d2a3688d9637c3b8a233e6c827c6bff
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-29 18:12:02 +0100

    changed max runtime in submit file to 48h

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=adl_training
 #SBATCH --output=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/out_%j.txt
 #SBATCH --error=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/err_%j.txt
-#SBATCH --time=24:00:00
+#SBATCH --time=48:00:00
 #SBATCH --partition=earth-5
 #SBATCH --gres=gpu:a100:1
 #SBATCH --cpus-per-task=4
