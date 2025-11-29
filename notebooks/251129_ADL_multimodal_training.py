1c2b9cb98ae357f461d71985a2cc266c2419acb7
nicolasvogel <nicolas.vogel@hotmail.com>
2025-11-29 16:46:31 +0100

    tokenlengths

--- notebooks/251129_ADL_multimodal_training.py
+++ notebooks/251129_ADL_multimodal_training.py
@@ -553,7 +553,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
         # compute embeddings
         with torch.no_grad():
             for images, texts in dataloader:
-                inputs = processor(text=texts, images=images, padding=True, trunction=True, return_tensors="pt").to(DEVICE)
+                inputs = processor(text=texts, images=images, padding=True, trunction=True, max_length=77,  return_tensors="pt").to(DEVICE)
 
                 img = model.get_image_features(pixel_values=inputs["pixel_values"]) # get image features
                 txt = model.get_text_features(input_ids=inputs["input_ids"], attention_mask=inputs["attention_mask"]) # get text features
@@ -653,7 +653,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
         train_loss = 0
 
         for images, texts in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False): # loop over batches of training data
-            inputs = processor(images=images, text=texts, padding=True, trunction=True, return_tensors="pt").to(DEVICE) # prepare inputs
+            inputs = processor(images=images, text=texts, padding=True, trunction=True, max_length=77, return_tensors="pt").to(DEVICE) # prepare inputs
             outputs = model(**inputs) # forward pass
 
             logits_i = outputs.logits_per_image # image-to-text logits
@@ -677,7 +677,7 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, num_epoch
         # no grad for validation
         with torch.no_grad():
             for images, texts in val_loader: # loop over batches of validation data
-                inputs = processor(images=images, text=texts, padding=True, trunction=True, return_tensors="pt").to(DEVICE) # prepare inputs
+                inputs = processor(images=images, text=texts, padding=True, trunction=True, max_length=77,  return_tensors="pt").to(DEVICE) # prepare inputs
                 outputs = model(**inputs) # forward pass
 
                 logits_i = outputs.logits_per_image # image-to-text logits
