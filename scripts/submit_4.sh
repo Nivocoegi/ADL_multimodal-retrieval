8942d502d7dc2ca57892d237439b0b74592e895b
nicolasvogel <nicolas.vogel@hotmail.com>
2025-12-06 16:00:28 +0100

    changed recall calc (all 5 captions not only one

---
+++ scripts/submit_4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=adl_training
+#SBATCH --output=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/out_%j.txt
+#SBATCH --error=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/err_%j.txt
+#SBATCH --time=48:00:00
+#SBATCH --partition=earth-4
+#SBATCH --gres=gpu:l40s:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+
+# ----------------------------
+# Job starten
+# ----------------------------
+
+bash /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/scripts/train_job.sh
+
