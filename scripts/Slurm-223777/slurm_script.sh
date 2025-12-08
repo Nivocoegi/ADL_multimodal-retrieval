15bed762d9bb1566f310002b57baedc99afbf544
Nicolas Vogel <vognic01@login02.cm.cluster>
2025-12-08 11:15:27 +0100

    hpc commit

---
+++ scripts/Slurm-223777/slurm_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=adl_training
+#SBATCH --output=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/out_%j.txt
+#SBATCH --error=/cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/err_%j.txt
+#SBATCH --time=24:00:00
+#SBATCH --partition=earth-5
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+
+# ----------------------------
+# Job starten
+# ----------------------------
+bash /cfs/earth/scratch/vognic01/ADL_multimodal-retrieval/scripts/train_job.sh
